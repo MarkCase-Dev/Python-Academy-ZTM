9813bf71ef95edc2add49fbaeb4c074bfb5cde55
MarkCase <markwilliamcase@googlemail.com>
2022-05-23 15:28:15 +0100

    Data Types Numbers

---
+++ DataTypes-Numbers.py
@@ -0,0 +1,41 @@
+#Fundamental Data types - Numbers
+
+int 
+float
+bool
+str
+list
+tuple
+set
+dict
+
+#int and float
+print(type(6))
+print(type(2-4))
+print(type(2 * 4))
+
+# Float takes up more memory than integer
+# eg 10.56 - essentially stores the 10 and .56 in 2 different
+# locations
+# think of floating numbers like scientific notation
+
+print(type(0.0000))
+print(type(2 / 4))
+
+# Python automatically converts this to float
+print(type(20+1.1))
+print(20+1.1)
+print(1.1 + 20)
+
+# number to a power
+print(2 ** 3)
+# Returns an integer rounded down
+print(5 // 4) # = 1.25 python rounds down to 1
+# modulo - finds remainder after division
+print(5 % 4)
+
+#classes --> custom types
+
+#Specialized data types ---> from modules -- extensions to laungauge
+
+# none -- kind of like zero in math -- the absence of value.
