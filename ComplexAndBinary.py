ccda57d37bf5579f918a45c7cc6bc3e5f867d0e8
MarkCase <markwilliamcase@googlemail.com>
2022-05-23 17:03:20 +0100

    Python Basics - Complex and binary data type

---
+++ ComplexAndBinary.py
@@ -0,0 +1,10 @@
+# Complex data type
+
+# Eqivalent to a real number
+complex 
+
+# Returns the binary representation of an integer
+print(bin(255))
+
+# Return to base 10
+print(int('0b11111111', 2))
