11b582cc06722876f252ac878822e446fece1e64
MarkCase <markwilliamcase@googlemail.com>
2022-05-26 15:52:33 +0100

    augmented operators

---
+++ augmentedoperators.py
@@ -0,0 +1,14 @@
+# augmented assignment operator - shorthand form
+
+some_value = 5
+some_value += 2     # some_value = some_value + 2
+
+print(some_value)
+
+some_value -= 2    #some_value = some_value -2
+
+print(some_value)
+
+some_value *= 2
+
+print(some_value)
