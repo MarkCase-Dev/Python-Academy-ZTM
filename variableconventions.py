b6af4c43df3f61db2c8a1d804077acab8bed5f79
MarkCase <markwilliamcase@googlemail.com>
2022-05-26 10:36:43 +0100

    variable conventions

---
+++ variableconventions.py
@@ -0,0 +1,14 @@
+##### Variable naming conventions ######
+
+##########################################
+# snake case				 #
+# Start with lowercase or underscores    #
+# Letters, numbers, underscores          #
+# Case sensitive                         #
+# Don'nt overwrite keywords              #
+##########################################
+
+user_iq = 190			# snake case
+_user_name = "Kennedy"  	# Underscore sigifies a private var.
+
+
